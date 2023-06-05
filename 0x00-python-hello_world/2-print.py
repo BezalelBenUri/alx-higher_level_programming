efb1fbc536250672ffc9048be5f854d0780bc43a
PhenomenalAI <diamondinyang@gmail.com>
2023-06-05 08:12:58 -0700

    Python script that prints exactly...

---
+++ 0x00-python-hello_world/2-print.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+"""
+This is a Python script that prints the message:
+"Programming is like building a multilingual puzzle"
+"""
+
+print("Programming is like building a multilingual puzzle")
+
