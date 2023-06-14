8ed270ebb0c8b77cd86035dcd3f6bd92af474af3
PhenomenalAI <diamondinyang@gmail.com>
2023-06-14 04:20:13 -0700

    returns the number of keys in a dictionary

---
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def number_keys(a_dictionary):
+    """
+    Returns the number of keys in a dictionary.
+    Args:
+        a_dictionary: The dictionary.
+    Returns:
+        The number of keys in the dictionary.
+    """
+    # Get the length of the dictionary.
+    number_of_keys = len(a_dictionary)
+    # Return the number of keys.
+    return number_of_keys
