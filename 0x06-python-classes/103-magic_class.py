1bf607169b1174e0a99ac342eaa6620a3a6f3e61
PhenomenalAI <diamondinyang@gmail.com>
2023-06-27 18:16:32 -0700

    magical class

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -15,10 +15,10 @@ def __init__(self, radius=0):
             raise TypeError("radius must be a number")
         self.__radius = radius
 
-    def area(self):
+def area(self):
         """Return the area of the MagicClass."""
         return (self.__radius ** 2 * math.pi)
 
-    def circumference(self):
+def circumference(self):
         """Return The circumference of the MagicClass."""
         return (2 * math.pi * self.__radius)s
