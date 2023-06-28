9db5b71d077c9360521676f7966b904027e4fd60
PhenomenalAI <diamondinyang@gmail.com>
2023-06-27 17:35:28 -0700

    class Square that defines a square by: (based on 5-square.py)

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -33,8 +33,8 @@ class Square:
     @position.setter
     def position(self, value):
         """Setter method to set the position attribute"""
-        if type(value) != tuple or len(value) != 2 or
-        type(value[0]) != int or type(value[1]) != int \
+        if type(value) != tuple or len(value) != 2 or \
+                type(value[0]) != int or type(value[1]) != int \
                 or value[0] < 0 or value[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
         else:
