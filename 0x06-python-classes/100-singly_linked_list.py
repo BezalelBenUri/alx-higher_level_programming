aacf5313fc66bf6344e58ce0762c3864fb4fa92e
PhenomenalAI <diamondinyang@gmail.com>
2023-06-27 17:43:29 -0700

    node and singly lined list

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -54,7 +54,8 @@ class SinglyLinkedList:
             self.head = new_node
         else:
             current = self.head
-            while current.next_node is not None and value >= current.next_node.data:
+            while current.next_node is not None and\
+                    value >= current.next_node.data:
                 current = current.next_node
             new_node.next_node = current.next_node
             current.next_node = new_node
@@ -67,4 +68,3 @@ class SinglyLinkedList:
             nodes.append(str(current.data))
             current = current.next_node
         return '\n'.join(nodes)
-
