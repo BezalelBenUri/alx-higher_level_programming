ec2a6e520f3196290adcba3d1d5ee3846511389b
PhenomenalAI <diamondinyang@gmail.com>
2023-07-11 04:58:30 -0700

    reading file lines

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,15 +1,21 @@
 #!/usr/bin/python3
-""" function that returns the number of lines of a text file
-"""
+"""Defines a text file line-counting function."""
 
 
-def number_of_lines(filename=""):
-    """ function: number_of_lines
+def write_file(filename="", text=""):
     """
-    if filename == "" or type(filename) != str:
-        return 0
-    line_counter = 0
-    with open(filename, "r") as file_input:
-        for line in file_input:
-            line_counter += 1
-    return line_counter
+    Writes a string to a text file and returns the number of characters written.
+
+    Args:
+        filename (str): The name of the file.
+        text (str): The text to write to the file.
+
+    Returns:
+        int: The number of characters written.
+
+    """
+    num_characters = 0
+    with open(filename, 'w', encoding='utf-8') as file:
+        num_characters = file.write(text)
+    return num_characters
+
