e742b4f87be7ff92b44e94ad3d7a3b06daf08cd4
PhenomenalAI <diamondinyang@gmail.com>
2023-07-11 04:44:11 -0700

    reading file lines

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -8,8 +8,8 @@ def number_of_lines(filename=""):
     """
     if filename == "" or type(filename) != str:
         return 0
-    counter = 0
-    with open(filename, "r") as f:
-        for line in f:
-            counter += 1
-    return counter
+    line_counter = 0
+    with open(filename, "r") as file_input:
+        for line in file_input:
+            line_counter += 1
+    return line_counter
