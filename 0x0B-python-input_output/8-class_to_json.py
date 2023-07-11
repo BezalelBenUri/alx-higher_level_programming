eaee790b87cd475fce024253dad17c2a3cf1b233
PhenomenalAI <diamondinyang@gmail.com>
2023-07-11 11:23:52 -0700

    Class to JSON

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,22 +1,8 @@
+#!/usr/bin/python3
+
+"""Defines a Python class-to-JSON function."""
+
+
 def class_to_json(obj):
-    """
-    Returns the dictionary description with a
-    simple data structure for JSON serialization of an object.
-
-    Args:
-        obj: The object to be serialized.
-
-    Returns:
-        dict: The dictionary representation of the object.
-
-    """
-    attributes = obj.__dict__
-    if hasattr(obj, "__dict__"):
-        attributes = obj.__dict__.copy()
-
-    for key, value in attributes.items():
-        if isinstance(value, (list, dict, str, int, bool)):
-            continue
-        attributes[key] = str(value)
-
-    return attributes
+    """Return the dictionary represntation of a simple data structure."""
+    return obj.__dict__
