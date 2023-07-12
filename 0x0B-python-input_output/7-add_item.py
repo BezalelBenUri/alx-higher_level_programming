154ffc62cbb985fc7d9af956bd4c0a8d79d32b7e
PhenomenalAI <diamondinyang@gmail.com>
2023-07-12 06:27:52 -0700

    add items

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,18 +1,22 @@
 #!/usr/bin/python3
+"""module
+adds all arguments to a Python list
+"""
+from sys import argv
+import os
+load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
 
-"""Add all arguments to a Python list and save them to a file."""
-
-
-import sys
 
 if __name__ == "__main__":
-    save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
-    load_from_json_file = \
-        __import__('8-load_from_json_file').load_from_json_file
-
-    try:
-        props = load_from_json_file("add_item.json")
-    except FileNotFoundError:
-        props = []
-    props.extend(sys.argv[1:])
-    save_to_json_file(props, "add_item.json")
+    my_list = []
+    if len(argv) > 1:
+        for i in range(len(argv) - 1):
+            my_list.append(argv[i + 1])
+    if os.path.isfile(os.getcwd() + "/add_item.json"):
+        json_list = load_from_json_file("add_item.json")
+    else:
+        with open("add_item.json", encoding="utf-8", mode="w"):
+            json_list = []
+    new_json = json_list + my_list
+    save_to_json_file(new_json, "add_item.json")
