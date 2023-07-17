84a1352d8def023c4d728ac6bcd4013cd42eb2f7
PhenomenalAI <diamondinyang@gmail.com>
2023-07-17 10:55:30 -0700

    unitest

---
+++ 0x0C-python-almost_a_circle/tests/rectangle_tester.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Module to test the function rectangle"""
+import unittest
+import rectangle
+
+
+class TestRectangle(unittest.TestCase):
+    def rectangle_outputtester(self):
+        
+        """Function to test the output the function rectangle"""
+
+        self.assertAlmostEqual(rectangle(,)
+
+     def rectangle_inputester(self):
+        """Function to test the input the function rectangle"""
+
+        self.assertRaises(TypeError, rectangle, )
