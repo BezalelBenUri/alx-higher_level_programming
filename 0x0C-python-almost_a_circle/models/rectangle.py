e4e07050d1e4f64e963e090d7c166ef8f989ee06
PhenomenalAI <diamondinyang@gmail.com>
2023-07-17 01:51:55 -0700

    commit

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -14,4 +14,67 @@ class Rectangle(Base):
         self.height = height
         self.x = x
         self.y = y
+        
         super().__init__(id)
+
+    @property
+    def width(self):
+        """ Width Getter - Read Value """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """ Setter width - change value """
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        elif value <= 0:
+            raise ValueError("width must be > 0")
+        else:
+            self.__width = value
+
+    @property
+    def height(self):
+        """ height Getter - Read Value """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """ Setter height - change value """
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        elif value <= 0:
+            raise ValueError("height must be > 0")
+        else:
+            self.__height = value
+
+    @property
+    def x(self):
+        """ x Getter - Read Value """
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """ Setter x - change value """
+        if type(value) is not int:
+            raise TypeError("x must be an integer")
+        elif value < 0:
+            raise ValueError("x must be >= 0")
+        else:
+            self.__x = value
+
+    @property
+    def y(self):
+        """ y Getter - Read Value """
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        """ Setter y - change value """
+        if type(value) is not int:
+            raise TypeError("y must be an integer")
+        elif value < 0:
+            raise ValueError("y must be >= 0")
+        else:
+            self.__y = value
+
+
