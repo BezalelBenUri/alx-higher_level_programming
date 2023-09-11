fda656ed89028d059128d850c46d3e0500b15939
PhenomenalAI <diamondinyang@gmail.com>
2023-09-11 11:04:44 -0700

    computes and prints a factorial

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+const ar = process.argv.slice(2);
+const nur = parseInt(array[0]);
+if (isNaN(ar[0])) {
+  console.log(1);
+} else {
+  console.log(factorial(nur));
+}
+function factorial (n) {
+  if (n === 0) {
+    return 1;
+  } else {
+    return n * factorial(n - 1);
+  }
+}
