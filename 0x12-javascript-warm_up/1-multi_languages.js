ee491c125343185db081fdae90cb3bdc0130d2af
PhenomenalAI <diamondinyang@gmail.com>
2023-09-11 09:21:26 -0700

    simple prints

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,8 +1,2 @@
 #!/usr/bin/node
-
-const const10 = 'C is fun';
-const const11 = 'Python is cool';
-const const12 = 'Javascript is amazing';
-console.log(const10);
-console.log(const11);
-console.log(const12);
+console.log('C is fun\nPython is cool\nJavascript is amazing');
