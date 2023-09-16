d340663f0b940062019c9db44eacf55d831c1113
PhenomenalAI <diamondinyang@gmail.com>
2023-09-16 14:32:06 -0700

    takes in an argument and displays all values in the states table..

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -8,8 +8,9 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3], port=3306)
     curs = db.cursor()
-    curs.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'"
-                .format(sys.argv[4]))
+    curs.execute(
+            "SELECT * FROM states WHERE name LIKE BINARY '{}'"
+            .format(sys.argv[4]))
     result = curs.fetchall()
     for row in result:
         print(row)
