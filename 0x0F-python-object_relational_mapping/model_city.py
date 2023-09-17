1f80a84a00bb96c85b494c329a25abb57d03c200
PhenomenalAI <diamondinyang@gmail.com>
2023-09-16 17:00:34 -0700

    model city and print all city in db

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
-
-
-from sqlalchemy import Column, Integer, String, ForeingnKey
-from model_state import Base, State
+""" contains the class definition of a State and an instance """
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, ForeignKey
+from model_state import Base
 
 
 class City(Base):
-    '''
-    class city
-    '''
     __tablename__ = 'cities'
-    id = Column(Integer, primary_key=True)
+
+    id = Column(Integer, primary_key=True,
+                nullable=False)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey('states.id'))
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
