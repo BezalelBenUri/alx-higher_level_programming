cd538cb4587678b09e8d8b3b6c87e6baf970b986
PhenomenalAI <diamnondinyang@gmail.com>
2023-09-29 10:34:48 -0700

    Python script that takes in a URL and an email address, sends a POST..

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""  fetches https://intranet.hbtn.io/status  """
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    email = {'email': argv[2]}
+    rd = requests.post(argv[1], data=email)
+    print(rd.text)
