39e3043003a65e7a734779c188bf5ad48bfac129
PhenomenalAI <diamondinyang@gmail.com>
2023-10-24 10:33:17 -0700

    js gets the ontent of a webpage and stores in a filwqe

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
 const fs = require('fs');
 const reqs = require('request');
-reqs(process.argv[2]).pipe(fs.createWriterStream(process.argv[3]));
+reqs(process.argv[2]).pipe(fs.createWriteStream(process.argv[3]));
